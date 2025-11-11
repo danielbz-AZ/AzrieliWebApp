d9308e457e94c095197fe0ab1595f22ef64b2ba5
DanielB59FS <81916462+DanielB59FS@users.noreply.github.com>
2025-11-11 04:37:37 +0200

    Switch slick carousel to npm imports in gallery
    Commented out local slick carousel script and CSS includes in legogall.html and replaced them with npm package imports in gallery.js. This change improves dependency management and streamlines asset loading.

--- public/scripts/gallery.js
+++ public/scripts/gallery.js
@@ -1,3 +1,8 @@
+import $ from "jquery";
+import "slick-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 var sframe = "<div id='slide' class='slider-for'>";
 var slides = "<div id='gallery' class='slider-nav'>";
 var media = "<section id='media'>";
